0170e2d57a5b4784a6eac60d8ce3943fc15da825
Jeycher <jeycherescorcia@gmail.com>
2025-07-08 22:25:44 -0600

    parte Maykoll

---
+++ ProyectoProgramacion/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoProgramacion.Controllers
+{
+    public class AdminController : Controller
+    {
+        // Apartamentos
+        public ActionResult ApartamentosList()
+        {
+            return View();
+        }
+
+        public ActionResult ApartamentoForm(int? id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+
+        // Residentes
+        public ActionResult ResidentesList()
+        {
+            return View();
+        }
+
+        public ActionResult ResidenteForm(int? id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
