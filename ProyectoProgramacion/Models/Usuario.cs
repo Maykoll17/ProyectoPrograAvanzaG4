c8614e69583dcde7c3bd8bdfa49b0d97ef033e61
Gerson <gerschacon@gmail.com>
2025-07-17 22:44:29 -0600

    Vista principal, registro y solicitud

---
+++ ProyectoProgramacion/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoProgramacion.Models
+{
+    public class Usuario
+    {
+            [Required]
+            [EmailAddress]
+            public string Correo { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Contrasenna { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            [Compare("Contrasenna", ErrorMessage = "Las contraseñas no coinciden.")]
+            public string ConfirmarContraseña { get; set; }
+        }
+
+
+    
+}
